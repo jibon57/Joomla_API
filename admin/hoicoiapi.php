340b4a0209102ba08404d4998dfea066c530ecde
Jibon Lawrence Costa <jiboncosta57@gmail.com>
2016-03-19 18:12:51 +0600

    Token Security Added
    Token Security Added

--- admin/hoicoiapi.php
+++ admin/hoicoiapi.php
@@ -3,99 +3,105 @@
  * @version 1
  * @package    joomla
  * @subpackage Hoicoiapi
- * @author	   	
+ * @author
  *  @copyright  	Copyright (C) 2014, . All rights reserved.
- *  @license 
+ *  @license
  */
-
 //--No direct access
 defined('_JEXEC') or die('Resrtricted Access');
+JToolBarHelper::preferences('com_hoicoiapi', '550');
+$params = JComponentHelper::getParams('com_hoicoiapi');
+$token = (string) $params->get("token");
+$count = (int) strlen($token);
+if ($count < 52 || !$count = 52) {
+    JFactory::getApplication()->enqueueMessage("Please use at least 52 characters long token key. Click on Options button to add/change. ", "error");
+}
 ?>
-<h2>Here are some examples</h2>
+<h2>Here are some examples (Need to use token with every request. Check the first 2 examples.)</h2>
 <table>
-	<tr>
-		<td><b>Service</b></td>
-		<td><b>URL</b></td>
-	</tr>
-	<tr>
-		<td>Login</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=login&username=test&pass=test" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=login&username=test&pass=test</a></td>
-	</tr>
-	<tr>
-		<td>Registration</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=registration&name=NAME&username=USERNAME&passwd=PASSWORD&email=EMAIL" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=registration&name=NAME&username=USERNAME&passwd=PASSWORD&email=EMAIL</a></td>
-	</tr>
-	<tr>
-		<td>Article Categories</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getContents" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getContents</a></td>
-	</tr>
-	<tr>
-		<td>Article of a Category</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getContents&catid=2" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getContents&catid=2</a></td>
-	</tr>
-	<tr>
-		<td>Single Article</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getContents&id=1" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getContents&id=1</a></td>
-	</tr>
-	<tr>
-		<td>Virtuemart Categories</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getVM&lan=en_gb" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getVM&lan=en_gb</a></td>
-	</tr>
-	<tr>
-		<td>Virtuemart Products in a Category</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getVM&lan=en_gb&catid=6" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getVM&lan=en_gb&catid=6</a></td>
-	</tr>
-	<tr>
-		<td>Virtuemart Single Product</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getVM&lan=en_gb&id=62" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getVM&lan=en_gb&id=62</a></td>
-	</tr>
-	<tr>
-		<td>K2 Categories</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getK2" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getK2</a></td>
-	</tr>
-	<tr>
-		<td>K2 Items in a category</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getK2&catid=1" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getK2&catid=1</a></td>
-	</tr>
-	<tr>
-		<td>K2 Single Items</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getK2&id=2" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getK2&id=2</a></td>
-	</tr>
-	<tr>
-		<td>Easyblog Categories</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getEasyblog" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getEasyblog</a></td>
-	</tr>
-	<tr>
-		<td>Easyblog Posts in a category</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getEasyblog&catid=1" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getEasyblog&catid=1</a></td>
-	</tr>
-	<tr>
-		<td>Easyblog Single Post</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getEasyblog&id=1" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getEasyblog&id=1</a></td>
-	</tr>
-	<tr>
-		<td>Kunena Categories</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getkunena" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getkunena</a></td>
-	</tr>
-	<tr>
-		<td>Kunena Items in a category</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getkunena&catid=1" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getkunena&catid=1</a></td>
-	</tr>
-	<tr>
-		<td>Kunena Single Items</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getkunena&id=2" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getkunena&id=2</a></td>
-	</tr>
-	<tr>
-		<td>HikaShop Categories</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getHika" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getHika</a></td>
-	</tr>
-	<tr>
-		<td>HikaShop Items in a category</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getHika&catid=1" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getHika&catid=1</a></td>
-	</tr>
-	<tr>
-		<td>HikaShop Single Items</td>
-		<td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getHika&id=2" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getHika&id=2</a></td>
-	</tr>
-</table>	
+    <tr>
+        <td><b>Service</b></td>
+        <td><b>URL</b></td>
+    </tr>
+    <tr>
+        <td>Login</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=login&username=test&pass=test&token=<?php echo $token; ?>" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=login&username=test&pass=test&token=<?php echo $token; ?></a></td>
+    </tr>
+    <tr>
+        <td>Registration</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=registration&name=NAME&username=USERNAME&passwd=PASSWORD&email=EMAIL&token=<?php echo $token; ?>" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=registration&name=NAME&username=USERNAME&passwd=PASSWORD&email=EMAIL&token=<?php echo $token; ?></a></td>
+    </tr>
+    <tr>
+        <td>Article Categories</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getContents" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getContents</a></td>
+    </tr>
+    <tr>
+        <td>Article of a Category</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getContents&catid=2" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getContents&catid=2</a></td>
+    </tr>
+    <tr>
+        <td>Single Article</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getContents&id=1" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getContents&id=1</a></td>
+    </tr>
+    <tr>
+        <td>Virtuemart Categories</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getVM&lan=en_gb" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getVM&lan=en_gb</a></td>
+    </tr>
+    <tr>
+        <td>Virtuemart Products in a Category</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getVM&lan=en_gb&catid=6" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getVM&lan=en_gb&catid=6</a></td>
+    </tr>
+    <tr>
+        <td>Virtuemart Single Product</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getVM&lan=en_gb&id=62" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getVM&lan=en_gb&id=62</a></td>
+    </tr>
+    <tr>
+        <td>K2 Categories</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getK2" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getK2</a></td>
+    </tr>
+    <tr>
+        <td>K2 Items in a category</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getK2&catid=1" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getK2&catid=1</a></td>
+    </tr>
+    <tr>
+        <td>K2 Single Items</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getK2&id=2" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getK2&id=2</a></td>
+    </tr>
+    <tr>
+        <td>Easyblog Categories</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=easyblog_categories" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=easyblog_categories</a></td>
+    </tr>
+    <tr>
+        <td>Easyblog Posts in a category</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=easyblog_posts&catid=1" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=easyblog_posts&catid=1</a></td>
+    </tr>
+    <tr>
+        <td>Easyblog Single Post</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=easyblog_single_post&id=1" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=easyblog_single_post&id=1</a></td>
+    </tr>
+    <tr>
+        <td>Kunena Categories</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getkunena" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getkunena</a></td>
+    </tr>
+    <tr>
+        <td>Kunena Items in a category</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getkunena&catid=1" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getkunena&catid=1</a></td>
+    </tr>
+    <tr>
+        <td>Kunena Single Items</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getkunena&id=2" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getkunena&id=2</a></td>
+    </tr>
+    <tr>
+        <td>HikaShop Categories</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getHika" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getHika</a></td>
+    </tr>
+    <tr>
+        <td>HikaShop Items in a category</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getHika&catid=1" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getHika&catid=1</a></td>
+    </tr>
+    <tr>
+        <td>HikaShop Single Items</td>
+        <td><a href="<?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getHika&id=2" target="_blank"><?php echo JURI::root(); ?>index.php?option=com_hoicoiapi&task=getHika&id=2</a></td>
+    </tr>
+</table>
 
--- site/hoicoiapi.php
+++ site/hoicoiapi.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package    hoicoiapi
  * @subpackage Base
@@ -6,16 +7,21 @@
  * @author     Created on 14-Sep-2014
  * @license    GNU/GPL
  */
-
 //-- No direct access
 defined('_JEXEC') || die('=;)');
 
 
-//-- Get an instance of the controller with the prefix 'hoicoiapi'
+$params = JComponentHelper::getParams('com_hoicoiapi');
+$token = trim($params->get('token'));
+$get = trim(JFactory::getApplication()->input->get('token', '0', 'STRING'));
+
+if (!$token || !strcmp($token, $get) == 0) {
+    jexit('Please use correct token with URL');
+}
+
+
 $controller = JControllerLegacy::getInstance('hoicoiapi');
 
-//-- Execute the 'task' from the Request
-$controller->execute(JFactory::getApplication()->input->get('task'));
+$controller->execute(JFactory::getApplication()->input->get('task', 'getContents'));
 
-//-- Redirect if set by the controller
 $controller->redirect();
